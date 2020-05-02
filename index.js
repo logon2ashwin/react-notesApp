26e983af70184410826fcbc24c5ef8fe7a888290
Baskaran Ashwin <ashwin.baskaran@wallstreetenglish.com>
2020-05-02 20:22:47 +0530

    ESlint, webpack and base react setup

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HomeComponent from './src/index';
+
+ReactDOM.render(
+  <HomeComponent>test</HomeComponent>,
+  document.getElementById('app')
+);
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HomeComponent = (props) => {
+  return (
+    <div></div>
+  );
+};
+
+export default HomeComponent;
