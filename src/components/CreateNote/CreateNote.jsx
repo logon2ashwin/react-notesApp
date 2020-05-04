5f5933e01b8b78359317af6be33c785470ff519e
Baskaran Ashwin <ashwin.baskaran@wallstreetenglish.com>
2020-05-05 00:06:31 +0530

    UI done for create note module

--- src/components/CreateNote/CreateNote.jsx
+++ src/components/CreateNote/CreateNote.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CreateNote.scss';
 
 const CreateNote = props => {
+  const [title,setTitle] = useState('');
   return (
     <div className='create-note'>
       <form className='note-form-container'>
         <div className='note-title-holder'>
-          <input className='note-title-input' type='text' name='name' placeholder="Title" />
+          <div className='note-input-with-label'>
+            <input className='note-title-input' type='text' value={title} onChange={event => setTitle(event.target.value)} required/>
+            <label className='note-title-label' >Title</label>
+          </div>
         </div>
         <div className='note-body-holder'>
-          <textarea className='note-text-area' name='notes' placeholder="Notes"></textarea>
+          <textarea className='note-text-area' name='notes' placeholder='Notes'></textarea>
         </div>
-        <div className="note-button-grid">
-          <div className="note-button-holder">
+        <div className='note-button-grid'>
+          <div className='note-button-holder'>
             <button>Save</button>
-            <button>Cancel</button>
+            <button onClick={props.toggleCreateNote}>Cancel</button>
           </div>
         </div>
       </form>
